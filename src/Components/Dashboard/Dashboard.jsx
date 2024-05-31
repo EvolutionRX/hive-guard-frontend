74dda8d2579e39dbfa27379dc091bc67aaad470c
EvolutionRX <tmevolution1@gmail.com>
2024-05-31 19:27:27 -0300

    fix 1

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,37 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import '../../App.css';
+import React from 'react'
+import '../../App.css'
+import Sidebar from '../Dashboard/SideBarSection/Sidebar'
+import Body from '../Dashboard/BodySection/Body'
 
 const Dashboard = () => {
-    const [temperature, setTemperature] = useState(null);
-    const [humidity, setHumidity] = useState(null);
-    const [beeCount, setBeeCount] = useState(null);
-
-    useEffect(() => {
-        // Simulate data fetching
-        setTemperature(22.5); // Replace with actual data fetching
-        setHumidity(55); // Replace with actual data fetching
-        setBeeCount(150); // Replace with actual data fetching
-    }, []);
-
     return (
         <div className="dashboard flex">
             <div className="dashboardContainer flex">
-                <div className="dataCard">
-                    <h2>Temperature</h2>
-                    <p>{temperature} °C</p>
-                </div>
-                <div className="dataCard">
-                    <h2>Humidity</h2>
-                    <p>{humidity} %</p>
-                </div>
-                <div className="dataCard">
-                    <h2>Bee Count</h2>
-                    <p>{beeCount}</p>
-                </div>
+                <Sidebar/>
+                <Body/>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Dashboard;
-
+export default Dashboard
