395431ad739eef7f9662fccabda309fed09a1e4a
EvolutionRX <tmevolution1@gmail.com>
2024-06-19 01:13:18 -0300

    add screen for view alerts

--- src/Components/Dashboard/BodySection/ActivitySection/Activity.jsx
+++ src/Components/Dashboard/BodySection/ActivitySection/Activity.jsx
@@ -7,37 +7,20 @@ import user from '../../../../Assets/logo.png';
 const Activity = () => {
   const [alerts, setAlerts] = useState([]);
   const serverIP = localStorage.getItem('serverIP');
-   // Mock data for alerts
-   const mockAlerts = [
-    {
-      sensorId: "esp32cam738785",
-      alertType: "HUM",
-      value: "70.20"
-    },
-    {
-      sensorId: "esp32cam738785",
-      alertType: "HUM",
-      value: "70.33"
-    },
-    {
-      sensorId: "esp32cam738785",
-      alertType: "HUM",
-      value: "70.52"
-    }
-  ];
+  const [showModal, setShowModal] = useState(false);
+  
 
   useEffect(() => {
     const pollAlerts = async () => {
       try {
-        //const response = await Axios.get(`${serverIP}/api/alerts`);
-        const response = { data: mockAlerts };
+        const response = await Axios.get(`${serverIP}/api/alerts`);
         if (response.data.length > 0) {
           setAlerts(response.data);
         } else {
           setAlerts([]);
         }
       } catch (error) {
-        console.error('Error fetching alerts:', error);
+        console.error('Ocurrió un error al buscar las alertas:', error);
       }
     };
 
@@ -45,21 +28,21 @@ const Activity = () => {
     return () => clearInterval(interval); // Clean up the interval on component unmount
   }, [serverIP]);
 
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
   return (
     <div className="activitySection">
       <div className="heading flex">
         <h1>Actividad reciente</h1>
-        <button className="btn flex">
+        <button className="btn flex" onClick={openModal}>
           Mirar todas
           <BsArrowRightShort className="icon" />
         </button>
       </div>
 
       <div className="secContainer grid">
-        {/* <div className="singleCustomer flex"> */}
-        
-
-        {alerts.length > 0 && alerts.map((alert, index) => (
+      {alerts.length > 0 && alerts.slice(0, 3).map((alert, index) => (
           <div key={index} className="singleCustomer flex">
             <img src={user} alt="Customer Image" />
             <div className="customerDetails">
@@ -76,6 +59,33 @@ const Activity = () => {
           </div>
         ))}
       </div>
+
+      {showModal && (
+        <div className="modalOverlay">
+          <div className="modalContent">
+            <button className="closeButton" onClick={closeModal}>×</button>
+            <h2>Alertas</h2>
+            <div className="secContainer grid">
+            {alerts.map((alert, index) => (
+              <div key={index} className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                  <span className="name">{alert.alertType === 'TEMP' ? 'Temperatura' : 'Humedad'}</span>
+                  <small>
+                    {alert.alertType === 'TEMP'
+                      ? `Subió la temperatura por encima de ${alert.value}°`
+                      : `Subió la humedad por encima de ${alert.value}%`}
+                  </small>
+                </div>
+                <div className="duration">
+                  Hace un momento
+                </div>
+              </div>
+            ))}
+          </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
